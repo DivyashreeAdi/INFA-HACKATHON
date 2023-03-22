1a088902cd6fb0ba64e43c77a0ca5dfc582991fd
Mayukh Mousam Mishra <61306993+mm7104@users.noreply.github.com>
2023-03-22 15:37:15 +0530

    Add files via upload

---
+++ chatgpt.py
@@ -0,0 +1,23 @@
+secret_key = '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'
+
+#print(prompt)
+import openai
+openai.api_key = secret_key
+
+def test():
+    while True:
+        prompt = input()
+        output = openai.Completion.create(
+            model = 'text-davinci-003',
+            prompt = prompt,
+            max_tokens= 1000,
+            temperature =0
+        )
+
+        output_text = output['choices'][0]['text']
+
+        print(output_text)
+    
+test()
+
+    #print(output)
