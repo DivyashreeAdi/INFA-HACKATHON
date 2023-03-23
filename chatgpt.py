9e1134fbb7388b2bf89433e17e573ff1f4cad9f2
Mayukh Mousam Mishra <61306993+mm7104@users.noreply.github.com>
2023-03-23 12:57:08 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,27 +1,23 @@
-
-
 import openai
-import os
 from flask import Flask
-#from env import secret_key
+import os
 
-openai.api_key = os.environ.get('secret_key')
+openai.api_key = os.environ['secret_key']
 app = Flask(__name__)
 
 @app.route('/')
 def test():
-    while True :
-        prompt = input("Enter your prompt: ")
-        output = openai.Completion.create(
-            model='text-davinci-003',
-            prompt=prompt,
-            max_tokens=1000,
-            temperature=0
-        )
-        output_text = output['choices'][0]['text']
-        return output_text
+    prompt = input("Enter your prompt: ")
+    output = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1000,
+        temperature=0
+    )
+    output_text = output['choices'][0]['text']
+    return output_text
 
 if __name__ == '__main__':
-    app.debug=True
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
